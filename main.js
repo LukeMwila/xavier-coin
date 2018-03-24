f4911579c2bc0ba168bf555e56b6da6626ef5e88
Luke Mwila <outlier.developer@gmail.com>
2018-03-24 11:30:06 +0200

    Created and updated Block class

---
+++ main.js
@@ -0,0 +1,21 @@
+class Block{
+    /**
+     * 
+     * @param {*} index - the position of the block on the chain
+     * @param {*} timestamp - the timestamp of the blocks creation
+     * @param {*} data - includes any type of data we want to associate with this block. In case of a currency,
+     * want to store details of how much money was transferred, the sender and the receiver
+     * @param {*} previousHash  - contains the hash of the previous block. This maintains the integrity of our blockchain
+     */
+    constructor(index, timestamp, data, previousHash = ''){
+        this.index = index
+        this.timestamp = timestamp 
+        this.data = data 
+        this.previousHash = previousHash
+        this.hash = ''
+    }
+
+    calculateHash(){
+        
+    }
+}
