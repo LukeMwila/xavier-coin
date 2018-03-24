b79def1d14c00db048a9190e447c7f289c26ad51
Luke Mwila <outlier.developer@gmail.com>
2018-03-24 12:08:49 +0200

    Test validity of blockchain after altering data

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const moment = require ('moment')
 const SHA256 = require('crypto-js/sha256')
 
 class Block{
@@ -21,3 +22,63 @@ class Block{
         return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString()
     }
 }
+
+class BlockChain{
+    constructor(){
+        this.chain = [this.createGenesisBlock()]
+    }
+
+    createGenesisBlock(){
+        return new Block(0, this.getTimeStamp(), 'Genesis block', '0')
+    }
+
+    getLatestBlock(){
+        return this.chain[this.chain.length - 1]
+    }
+
+    getTimeStamp(){
+        return moment(new Date()).toISOString()
+    }
+
+    getNextIndex(){
+        return this.chain.length
+    }
+
+    addBlock(newBlock){
+        newBlock.previousHash = this.getLatestBlock().hash
+        newBlock.hash = newBlock.calculateHash()
+        this.chain = [...this.chain, newBlock]
+    }
+
+    isChainValid(){
+        for(let i = 1; i < this.chain.length; i++){
+            const currentBlock = this.chain[i]
+            const previousBlock = this.chain[i - 1]
+
+            if(currentBlock.hash !== currentBlock.calculateHash()){
+                return false
+            }
+
+            if(currentBlock.previousHash !== previousBlock.hash){
+                return false
+            }
+
+            return true
+        }
+    }
+}
+
+/**
+ * Let's add some blocks to our blockchain!
+ */
+
+let xavierCoin = new BlockChain()
+xavierCoin.addBlock(new Block(xavierCoin.getNextIndex(), xavierCoin.getTimeStamp(), { amountTransferred: 112 }))
+xavierCoin.addBlock(new Block(xavierCoin.getNextIndex(), xavierCoin.getTimeStamp(), { amountTransferred: 12 }))
+xavierCoin.addBlock(new Block(xavierCoin.getNextIndex(), xavierCoin.getTimeStamp(), { amountTransferred: 52 }))
+
+xavierCoin.chain[1].data.amountTransferred = 3400
+
+console.log('Is blockchain valid? ' + xavierCoin.isChainValid())
+
+// console.log(JSON.stringify(xavierCoin, null, 4))
